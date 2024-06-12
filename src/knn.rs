4e8a448256ffc24aa76aeb50de4d153cc6adc94f
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-06-12 13:50:37 -0300

    Extracting column data

--- src/knn.rs
+++ src/knn.rs
@@ -1,10 +1,12 @@
 pub mod knn {
-    use core::f64;
+    use core::panic;
+    use std::fs::File;
 
+    use csv::Reader;
     use ndarray::Array1;
 
     pub fn calculate_distance(a: &Array1<f64>, b: &Array1<f64>) -> f64 {
-        
+
         let sum: f64 = a.iter()
             .zip(b.iter())
             .map(|(x, y)| (x - y).powf(2.0))
@@ -13,28 +15,67 @@ pub mod knn {
         f64::sqrt(sum as f64)
     }
 
-    pub fn find_knn(k: i32, item: &Array1<f64>) -> Vec<Option<Array1<f64>>> {
-        
-        let a = Array1::from_vec([5.1, 3.5, 1.4, 0.2].to_vec());
-        let b = Array1::from_vec([5.0, 3.3, 1.4, 0.2].to_vec());
-        let c = Array1::from_vec([7.0, 3.2, 4.7, 1.4].to_vec());
-        let d = Array1::from_vec([5.7, 2.8, 4.1, 1.3].to_vec());
-        let e = Array1::from_vec([6.3, 3.3, 6.0, 2.5].to_vec());
-        let f = Array1::from_vec([5.9, 3.0, 5.1, 1.8].to_vec());
+    pub fn find_knn(_k: i32, _item: &Array1<f64>, csv_reader: &mut Reader<File>) -> Vec<Option<Array1<f64>>> {
+    
+        let mut idx = 0;
+        for res in csv_reader.records() {
 
-        let dataset = [a, b, c, d, e, f];
+            idx += 1;
+            println!("Line: {idx}");
 
-        let mut distances: Vec<(f64, Array1<f64>)> = dataset
-            .iter()
-            .map(|data| (calculate_distance(item, data), data.clone()))
-            .collect();
-        
-        distances.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(std::cmp::Ordering::Equal));
-        
-        let knn: Vec<Option<Array1<f64>>> = (0..k)
-            .map(|key| distances.get(key as usize).map(|(_, data)| data.clone()))
-            .collect();
-        
-        knn
+                let r = res.unwrap();
+
+            let sepal_length_str = String::from(r.get(0).unwrap());
+            let sepal_length: f64 = match sepal_length_str.trim().parse() {
+                Ok(f) => f,
+                Err(_) =>
+                    panic!("Error converting '{}' to f64", sepal_length_str),
+            };
+
+            let sepal_width_str = String::from(r.get(1).unwrap());
+            let sepal_width: f64 = match sepal_width_str.trim().parse() {
+                Ok(f) => f,
+                Err(_) =>
+                    panic!("Error converting '{}' to f64", sepal_width_str),
+            };
+
+            let petal_length_str = String::from(r.get(2).unwrap());
+            let petal_length: f64 = match petal_length_str.trim().parse() {
+                Ok(f) => f,
+                Err(_) =>
+                    panic!("Error converting '{}' to f64", petal_length_str),
+            };
+
+            let petal_width_str = String::from(r.get(3).unwrap());
+            let petal_width: f64 = match petal_width_str.trim().parse() {
+                Ok(f) => f,
+                Err(_) =>
+                    panic!("Error converting '{}' to f64", petal_width_str),
+            };  
+
+            let class = String::from(r.get(4).unwrap());
+
+            println!("sepal_length: {}", sepal_length);
+            println!("sepal_width: {}", sepal_width);
+            println!("petal_length: {}", petal_length);
+            println!("petal_width: {}", petal_width);
+            println!("class: {}", class);
+        }
+
+        /*
+           let mut distances: Vec<(f64, Array1<f64>)> = dataset
+           .iter()
+           .map(|data| (calculate_distance(item, data), data.clone()))
+           .collect();
+
+           distances.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(std::cmp::Ordering::Equal));
+
+           let knn: Vec<Option<Array1<f64>>> = (0..k)
+           .map(|key| distances.get(key as usize).map(|(_, data)| data.clone()))
+           .collect();
+
+           knn */
+
+        todo!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
 
+use crate::knn::knn::find_knn;
+mod knn;
+
 use csv::ReaderBuilder;
+use ndarray::Array1;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
@@ -8,13 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // let headers = vec!["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Class"];
    
     // Defining the reader
-    let mut reader = ReaderBuilder::new()
-        .from_path("iris.csv")?;
-     
-    for rec in reader.records() {
-        println!("{:?}", rec?);
-    }
+    let mut reader = ReaderBuilder::new().from_path("iris.csv")?;
 
-    Ok(())
+    find_knn(3, &Array1::from_vec(vec![1., 2., 3.]), &mut reader);
+
+   Ok(())
 
 }
