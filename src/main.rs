6f8bf45c13ab2b8a2e4b9ff8fcef741bcb4205b8
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-07-10 11:46:39 -0300

    Add Iris data preprocessing for classification

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Defining the reader
     let mut reader = ReaderBuilder::new().from_path("iris.csv")?;
 
-    find_knn(3, &Array1::from_vec(vec![1., 2., 3.]), &mut reader);
+    find_knn(100, &Array1::from_vec(vec![6.1,2.8,5.0,1.8]), &mut reader);
 
    Ok(())
 
