6f250ab8eac38085da409a4a9bad4e0e7dcc0ebc
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-07-16 14:38:47 -0300

    Feat: add input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, io::{self, Write}};
 
 use crate::knn::knn::find_knn;
 mod knn;
@@ -8,14 +8,43 @@ use ndarray::Array1;
 
 fn main() -> Result<(), Box<dyn Error>> {
 
-    // Defining headers
-    // let headers = vec!["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Class"];
-   
-    // Defining the reader
     let mut reader = ReaderBuilder::new().from_path("iris.csv")?;
 
-    find_knn(100, &Array1::from_vec(vec![6.1,2.8,5.0,1.8]), &mut reader);
+    let (sepal_length, sepal_width, petal_length, petal_width) = get_input_values()?;
+    
+    let sepal_length = sepal_length.trim().parse::<f64>()?;
+    let sepal_width = sepal_width.trim().parse::<f64>()?;
+    let petal_length = petal_length.trim().parse::<f64>()?;
+    let petal_width = petal_width.trim().parse::<f64>()?;
 
-   Ok(())
+    find_knn(6, &Array1::from_vec(vec![sepal_length, sepal_width, petal_length, petal_width]), &mut reader);
 
+    Ok(())
 }
+
+
+fn get_input_values() -> Result<(String, String, String, String), Box<dyn Error>> {
+    let mut sepal_length = String::new();
+    let mut sepal_width = String::new();
+    let mut petal_length = String::new();
+    let mut petal_width = String::new();
+
+    print!("Sepal length: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut sepal_length)?;
+
+    print!("Sepal width: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut sepal_width)?;
+
+    print!("Petal length: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut petal_length)?;
+
+    print!("Petal width: ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut petal_width)?;
+
+    Ok((sepal_length.trim().to_string(), sepal_width.trim().to_string(), petal_length.trim().to_string(), petal_width.trim().to_string()))
+}
+
